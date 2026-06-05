2c0f274960545fb0aea7392a44e23aec7ed60a7c
Martin Velasquez <mvelasquez0211@gmail.com>
2026-06-05 11:54:03 -0300

    usar env firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7UdHlJ7EsIjBF-PydqFI0R_-jfRDJdsc",
-  authDomain: "pedido-app-c480a.firebaseapp.com",
-  projectId: "pedido-app-c480a",
-  storageBucket: "pedido-app-c480a.firebasestorage.app",
-  messagingSenderId: "374591751717",
-  appId: "1:374591751717:web:ec7db5d8f1d22e7577a1cc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
